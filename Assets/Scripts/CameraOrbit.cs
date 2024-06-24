eaa19814032713ae13f266fcbf8b7dc76a7dfc25
NikiBober <95879723+NikiBober@users.noreply.github.com>
2024-06-24 14:35:22 +0300

    v 0.2 Garage scene cameras and camera orbit
    ~ 2 hrs.

---
+++ Assets/Scripts/CameraOrbit.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraOrbit : MonoBehaviour
+{
+    [SerializeField] private Transform _targetTransform;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _distance = 5f;
+
+    private void OnMove(InputValue movementValue)
+    {
+        float inputX = movementValue.Get<Vector2>().x * _speed * Time.deltaTime;
+        float inputY = movementValue.Get<Vector2>().y * _speed * Time.deltaTime;
+
+        transform.position = _targetTransform.position;
+        transform.Rotate(Vector3.up, inputX, Space.World);
+        transform.Rotate(Vector3.right, -inputY, Space.Self);
+        transform.Translate(Vector3.back * _distance);
+    }
+}
