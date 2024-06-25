bfe3b8631c1d27c341d367d755bd7d2754c00962
NikiBober <95879723+NikiBober@users.noreply.github.com>
2024-06-25 20:58:44 +0300

    v 0.3.2 SceneLoader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private GameObject _loadingScreen;
+    [SerializeField] private Slider _progressBar;
+    [SerializeField] private TextMeshProUGUI _progressText;
+
+    public void LoadScene(string sceneToLoad)
+    {
+        _loadingScreen.SetActive(true);
+        StartCoroutine(LoadSceneRoutine(sceneToLoad));
+    }
+
+    private IEnumerator LoadSceneRoutine(string sceneToLoad)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToLoad);
+
+        while (!asyncLoad.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f) * 100;
+
+            _progressBar.value = progress;
+            _progressText.text = $"Loading... {Mathf.Round(progress)}%";
+
+            yield return null;
+        }
+
+        _loadingScreen.SetActive(false);
+    }
+}
